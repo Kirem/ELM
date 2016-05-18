a712a6243ab91c92590464ba700e4028c6dca600
doma93 <d.zieba93@gmail.com>
2016-05-18 22:36:15 +0200

    reading data without unknown

--- src/main/java/pl/edu/pwr/Starter.java
+++ src/main/java/pl/edu/pwr/Starter.java
@@ -20,19 +20,29 @@ public class Starter {
 
     public static void main(String[] args) {
         ElmConfig config = new ElmConfig();
-        config.numberOfNodes = 4;
-        config.numberOfInputClasses = 4;
+        config.numberOfNodes = 15;
+//        List<IrisModel> irisModels = DataCreator.readIrisData();
+//        double [][] inputs = DataCreator.createInputs(irisModels);
+//        config.numberOfInputClasses = inputs[0].length;
+//        config.numberOfOutputClasses = 1;
+//        double [] doubles = DataCreator.createOutputs(irisModels);
+
+        DataCreator.readData("iris.txt");
+//        DataCreator.readData("mammographicDataSet.txt");
+
+        double[][] inputs = DataCreator.getDataInputRows();
+        double[] doubles = DataCreator.getOutputs();
         config.numberOfOutputClasses = 1;
-        List<IrisModel> irisModels = DataCreator.readIrisData();
-        double [][] inputs = DataCreator.createInputs(irisModels);
-        double [] doubles = DataCreator.createOutputs(irisModels);
+        config.numberOfInputClasses = DataCreator.getNumberOfInputsClasses();
 
         ElmData elmData = new ElmData(inputs, doubles);
         ELM elm = new ELM(config, elmData);
         elm.train();
-        System.out.println(elm.test(new double[]{5.7,3.0,4.2,1.2}));
-        System.out.println(elm.test(new double[]{5.1,2.0,4.1,0.2}));
-        System.out.println(elm.test(new double[]{5.0,4.0,3.2,2.2}));
-        System.out.println(elm.test(new double[]{3.7,1.0,1.2,0.2}));
+
+//        IRIS TEST DATA
+//        System.out.println(elm.test(new double[]{5.1,3.5,1.4,0.2}));
+//        System.out.println(elm.test(new double[]{7.0,3.2,4.7,1.4}));
+//        System.out.println(elm.test(new double[]{5.8,2.7,5.1,1.9}));
+//        System.out.println(elm.test(new double[]{6.3,3.3,6.0,2.5}));
     }
 }
--- src/main/java/pl/edu/pwr/elm/DataCreator.java
+++ src/main/java/pl/edu/pwr/elm/DataCreator.java
@@ -9,9 +9,73 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DataCreator {
+    double [][] inputs;
+    double [] outputs;
+
+    private static List<double[]> dataInputRows;
+    private static List<String> outputsNamesList;
+    private static double [] outputsAsDouble;
 
     public DataCreator() {
 
+    }
+
+    public static void readData(String filePath) {
+        final Scanner in;
+        dataInputRows = new ArrayList<>();
+        outputsNamesList = new ArrayList<>();
+//        final List<IrisModel> irisModelsList= new ArrayList<IrisModel>();
+        try {
+            in = new Scanner(new FileReader(filePath));
+            while (in.hasNext()) {
+                final String[] columns = in.next().split(",");
+                double[] values = new double[columns.length - 1];
+                for(int i=0; i < columns.length - 1; i++) {
+                    values[i] = Double.parseDouble(columns[i]);
+                }
+                dataInputRows.add(values);
+                outputsNamesList.add(columns[columns.length-1]);
+
+//                final IrisModel irisModel = new IrisModel();
+//                irisModel.setSepalLength(Double.valueOf(columns[0]));
+//                irisModel.setSepalWidth(Double.valueOf(columns[1]));
+//                irisModel.setPetalLength(Double.valueOf(columns[2]));
+//                irisModel.setPetalWidth(Double.valueOf(columns[3]));
+//                irisModel.setIrisType(columns[4]);
+//                irisModelsList.add(irisModel);
+//                System.out.println("iris type: " + irisModel.getIrisType());
+            }
+            replaceResultNamesToNumbers();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+//        return irisModelsList;
+    }
+
+    public static void replaceResultNamesToNumbers() {
+        int typeIndex = 0;
+        outputsAsDouble = new double[outputsNamesList.size()];
+        outputsAsDouble[0] = typeIndex;
+        for(int i = 1; i < outputsNamesList.size(); i++) {
+            if(!outputsNamesList.get(i-1).equals(outputsNamesList.get(i))) {
+                typeIndex++;
+            }
+            outputsAsDouble[i] = typeIndex;
+        }
+    }
+
+    public static double[][] getDataInputRows(){
+        double[][] inputs = new double[dataInputRows.size()][];
+        dataInputRows.toArray(inputs);
+        return inputs;
+    }
+
+    public static double[] getOutputs() {
+        return outputsAsDouble;
+    }
+
+    public static int getNumberOfInputsClasses() {
+        return dataInputRows.get(0).length;
     }
 
     public static List<IrisModel> readIrisData() {
@@ -19,6 +83,7 @@ public class DataCreator {
         final List<IrisModel> irisModelsList= new ArrayList<IrisModel>();
         try {
             in = new Scanner(new FileReader("iris.txt"));
+            int j = 0;
             while (in.hasNext()) {
                 final String[] columns = in.next().split(",");
                 final IrisModel irisModel = new IrisModel();
@@ -41,6 +106,7 @@ public class DataCreator {
         for (int i = 0; i < irisModels.size(); i++) {
             double[] singleInput = {irisModels.get(i).getSepalLength(), irisModels.get(i).getSepalWidth(),
                     irisModels.get(i).getPetalLength(), irisModels.get(i).getPetalWidth()};
+
             inputs[i] = singleInput;
         }
         return inputs;
